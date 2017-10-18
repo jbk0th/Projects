4bd2e4564997b648e146394ece75404c1036804e
Jason Koth <jbkoth51@gmail.com>
2017-10-17 17:31:35 -0700

    Add files via upload

--- OPUS_Script_Conversion.py
+++ OPUS_Script_Conversion.py
@@ -5,60 +5,94 @@ import re
 from traits.api import *
 from traitsui.api import *
 
-class APS_Req_Updater():
+class APS_Req_Updater(HasTraits):
+    '''Below assay_folder and comp_assay_req_log are class level variable and every class will have the same values for these
+    just need to remember in writing the methods that inside of their definition hjas to refer with self.<attribute>'''
 
-    def update_assay_reqs(assay, field2):
-
-        assay_folder = {'GI':r'N:\Departments\R_D\NexGen\Assay Integration\04-APS File Requests\Gastro Intestinal Protocols\Requests',
+    def __init__(self):
+        self.assay_folder = {'GI':r'N:\Departments\R_D\NexGen\Assay Integration\04-APS File Requests\Gastro Intestinal Protocols\Requests',
                         'BCID-GP':r'N:\Departments\R_D\NexGen\Assay Integration\04-APS File Requests\BCID\BCID-GP\Requests',
                         'BCID-GN':r'N:\Departments\R_D\NexGen\Assay Integration\04-APS File Requests\BCID\BCID-GN\Verified Requests',
-                        'BCID-FP':r'N:\Departments\R_D\NexGen\Assay Integration\04-APS File Requests\BCID\BCID-FP\Verified Requests'
-                        'RP':r'N:\Departments\R_D\NexGen\Assay Integration\04-APS File Requests\RP\Requests'
+                        'BCID-FP':r'N:\Departments\R_D\NexGen\Assay Integration\04-APS File Requests\BCID\BCID-FP\Verified Requests',
+                        'RP':r'N:\Departments\R_D\NexGen\Assay Integration\04-APS File Requests\RP\Requests',
                         'LRTI':r'N:\Departments\R_D\NexGen\Assay Integration\04-APS File Requests\LRTI\Requests'}
-        comp_assay_req_log = {'GI':r'N:\Departments\R_D\NexGen\Assay Integration\04-APS File Requests\Gastro Intestinal Protocols\Protocols since Nov 2016.xlsx',
-                        'BCID-GP':r'N:\Departments\R_D\NexGen\Assay Integration\04-APS File Requests\BCID\06012017_BCID APS OPUS TRACKING SHEET.xlsx',
-                        'BCID-GN':r'N:\Departments\R_D\NexGen\Assay Integration\04-APS File Requests\BCID\06012017_BCID APS OPUS TRACKING SHEET.xlsx',
-                        'BCID-FP':r'N:\Departments\R_D\NexGen\Assay Integration\04-APS File Requests\BCID\06012017_BCID APS OPUS TRACKING SHEET.xlsx',
-                        'RP':r'N:\Departments\R_D\NexGen\Assay Integration\04-APS File Requests\RP\RP-Tracking-sheet-07102017.xlsx',
-                        'LRTI':r'N:\Departments\R_D\NexGen\Assay Integration\04-APS File Requests\LRTI\APS_Requests.xlsx'}
+        self.comp_assay_req_log = {'GI':r'M:\Sandbox\GI\Protocols since Nov 2016.xlsx',
+                        'BCID-GP':r'M:\Sandbox\BCID-GP\Request GP-000 BASELINE-GS-1.xlsx',
+                        'BCID-GN':r'M:\Sandbox\BCID-GN\GN-0007_BCID-GN-ControlEffectiveness-v2-1-TC03.xlsx',
+                        'BCID-FP':r'M:\Sandbox\BCID-FP\FP-009-FP-0026 opus.xlsx',
+                        'RP':r'M:\Sandbox\RP\RP-Tracking-sheet-07102017.xlsx'}
+
+        # {'GI':r'N:\Departments\R_D\NexGen\Assay Integration\04-APS File Requests\Gastro Intestinal Protocols\Protocols since Nov 2016.xlsx',
+        #                 'BCID-GP':r'N:\Departments\R_D\NexGen\Assay Integration\04-APS File Requests\BCID\06012017_BCID APS OPUS TRACKING SHEET.xlsx',
+        #                 'BCID-GN':r'N:\Departments\R_D\NexGen\Assay Integration\04-APS File Requests\BCID\06012017_BCID APS OPUS TRACKING SHEET.xlsx',
+        #                 'BCID-FP':r'N:\Departments\R_D\NexGen\Assay Integration\04-APS File Requests\BCID\06012017_BCID APS OPUS TRACKING SHEET.xlsx',
+        #                 'RP':r'N:\Departments\R_D\NexGen\Assay Integration\04-APS File Requests\RP\RP-Tracking-sheet-07102017.xlsx',
+        #                 'LRTI':r'N:\Departments\R_D\NexGen\Assay Integration\04-APS File Requests\LRTI\APS_Requests.xlsx'}
+
+    def update_assay_reqs(self, assay, field2='field'):
+
+        update_protocols = list()
+        aps_req_list = list()
+        _temp_log = list()
+        logged_opi = set()
+        opus_dev_log = list()
         ### Read a list of all protocols in a given directory
             #returns list of everything in current working directory
-        dir_list = os.listdir(assay_folder[assay])
-        aps_req_list = list()
+        dir_list = os.listdir(self.assay_folder[assay])
+
         for req in dir_list:
-        #    if 'GI' in req:
+           if assay in req:
                 aps_req_list.append(req)
         if len(aps_req_list) > 1:
-                print(aps_req_list)
-        else:
-            print('Error opening')
-        xl_protocol_log = load_workbook(filename = comp_assay_req_log[assay])
-        print(xl_protocol_log.get_sheet_names())
-        #update_protocols = list()
-#
-# ### Read in OPUS List from Excel Doc
-# # wb = protocols since Nov 2017 sheet, displays all sheet names
-# #xl_protocol_log.get_sheet_names()
-# # change to whatever sheet like to have active by sheet name, access like dic keys
-# prot_log_sheet = xl_protocol_log['Sheet1']
-# #, this returns tuple of all rows with contents or formatting
-# prot_log_cells = tuple(prot_log_sheet.rows)
-# #impt here the value on the cell object shows what
-# opus_dev_log = list()
-# #checks if the cell value isn't None and has 'GI' to only include only OPUS names w/ no trivial excel formatting
-# for cell in prot_log_cells:
-#     if cell[0].value is not None and 'GI' in cell[0].value:
-#         opus_dev_log.append(cell[0].value)
-#
-# logged_opi = set()
-# for entry in opus_dev_log:
-#     try:
-#         opus_req = re.match((".*(GI-\d+)-.*[^OB]"),entry)
-#         logged_opi.add(opus_req.groups(1)[0])
-#     except:
-#         continue
-# #only return the set of unique values from the list
-#
+                print(aps_req_list[0])
+
+        '''Reading from protocol log sheet to compare, what's listed in the
+        Request directory vs. what's already been logged. If any requests present that've
+        not been logged, record filenames to then take catalog impt info and write to
+        log sheet '''
+
+        # open Xl sheet to read in already logged requests.
+        comp_req_wb = load_workbook(self.comp_assay_req_log[assay])
+
+        print(comp_req_wb['Sheet1'].rows)
+        for cell in tuple(comp_req_wb['Sheet1'].rows):
+        #    print(cell[0].value)
+            if cell[0].value is not None and str(assay) in cell[0].value:
+                _temp_log.append(cell[0].value)
+        #checks if the cell value isn't None and has 'GI' to only include only OPUS names w/ no trivial excel formatting
+
+        '''IMPT detail here that however the assays are described/labeled here should be uniform
+        the discrepancy between how GI and RP were labeled threw an error'''
+
+        for entry in _temp_log:
+            # diagnostic tool print(assay,entry)
+            # try:
+            opus_req = re.match("({}-\\d*).*".format(assay),entry)
+            # diagnostic tool print(opus_req.groups(1))
+            logged_opi.add(opus_req.groups(1)[0])
+            # except:
+            #     continue
+        #diagnostic tool print(_temp_log[0], 'temp_log')
+        #diagnostic tool print(logged_opi)
+        #only return the set of unique values from the list
+
+        '''Need now open iteratively files, from the dri_list, with the reqs_to_update
+        that are not yet catalogged. Either join path or b/c already have list, go through
+        and if the pulled regex NOT in logged_opi. open to take info have correct list write
+        to the comp_assay_req_log'''
+
+        req_bk = load_workbook(self.comp_assay_req_log[assay])
+        req_sheet = req_bk.active
+        # print(req_bk.active)
+        # # for rows in req_sheet.rows:
+        # #     for cell in rows:
+        # #         print(cell.value)
+        #
+        # # Access specific values from
+        # req_sheet['D28'] = str(aps_req_list[0])
+        # req_bk.save(self.comp_assay_req_log[assay])
+
+
 # ### Filter out protocols already updated
 # reqs_to_update = list()
 # for entry in aps_req_list:
@@ -84,3 +118,6 @@ class APS_Req_Updater():
 #
 # # Access specific values from
 # req_sheet['C8'].value
+
+
+APS_Req_Updater().update_assay_reqs('RP')
